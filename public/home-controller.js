2f8720179fe1ea783701dd14d1ac6bc5513cbb8f
Daniel <dansolnik@gmail.com>
2018-02-24 16:47:14 -0600

    stuff worked

--- public/home-controller.js
+++ public/home-controller.js
@@ -3,7 +3,7 @@ myApp.controller('HomeCtrl', [
   '$resource',
   function ($scope, $resource) {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
+    detectedAlready = false;
     $scope.firstTime = true;
     let interval
     startInterval = function startInterval() {
@@ -14,25 +14,27 @@ myApp.controller('HomeCtrl', [
           interval = setInterval(function () {
             Webcam
               .snap(function (data_uri) {
-                // $.ajax({
-                //   url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/1e8b8aee-9349-4b61-b01e-525de7f8b4d8/image?iterationId=67026839-5d34-4cdf-b12c-4eae033b0cf2',
-                //   type: 'POST',
-                //   contentType: 'application/octet-stream',
-                //   processData: false,
-                //   headers: {
-                //     'Prediction-Key': 'faca39e5852f43a296bbb20466c0bbf4'
-                //   },
-                //   'data': makeblob(data_uri),
-                //   'success': function (res) {
-                //     console.log(res.Predictions);
-                //   }
-                // })
+                $.ajax({
+                  url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/1e8b8aee-9349-4b61-b01e-525de7f8b4d8/image?iterationId=8cc6bde7-21c7-4cda-bdf3-21c12533edf7',
+                  type: 'POST',
+                  contentType: 'application/octet-stream',
+                  processData: false,
+                  headers: {
+                    'Prediction-Key': 'faca39e5852f43a296bbb20466c0bbf4'
+                  },
+                  'data': makeblob(data_uri),
+                  'success': function (res) {
+                    console.log(res.Predictions);
+                  }
+                })
                 $.ajax({
                   'url': proxyurl + 'https://ekpqr3k4xi.execute-api.us-east-1.amazonaws.com/prod/myresource',
                   'type': 'PUT',
                   'contentType': "application/json",
                   'data': JSON.stringify({data: data_uri}),
                   'success': function (res) {
+                    if (detectedAlready) 
+                      return;
                     if (res == 'An intruder is in the home') {
                       $.ajax({
                         'url': '/text',
@@ -40,6 +42,7 @@ myApp.controller('HomeCtrl', [
                         'contentType': "application/json",
                         'data': JSON.stringify({number: '516-404-8254', image: data_uri})
                       });
+                      detectedAlready = true;
                     }
                   }
                 });

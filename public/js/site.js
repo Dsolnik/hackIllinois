9f8b8a32b26a2912503a667409126ba7cf63d871
Daniel <dansolnik@gmail.com>
2018-02-24 11:52:02 -0600

    api call working

--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,5 @@
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const rapid = new RapidAPI('coolproject', '71b145f659df4c68b9ae7e307fc96a77');
 $(document)
   .ready(function () {
     Webcam.attach('#my_camera');
@@ -9,7 +11,17 @@ $(document)
           Webcam
             .snap(function (data_uri) {
               console.log(data_uri);
-            })
+              $.ajax({
+                'url' : proxyurl + 'https://ekpqr3k4xi.execute-api.us-east-1.amazonaws.com/prod/myresource',
+                'type' : 'PUT',
+                'contentType' : "application/json",
+                'data' : JSON.stringify({data: data_uri}),
+              //The response from the server
+                'success' : function(res) {
+                  console.log(res);
+                }
+              });
+                })
         }, 3000);
       }
     }
@@ -21,8 +33,8 @@ $(document)
       }
     }
 
-    let start = jQuery('#start');
-    let stop = jQuery('#stop');
+    let start = $('#start');
+    let stop = $('#stop');
     start.click(startInterval)
     stop.click(stopInterval)
   });

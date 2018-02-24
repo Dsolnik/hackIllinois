40a59268cbedcca5e9fb52dcf92d422d332f202a
Daniel <dansolnik@gmail.com>
2018-02-24 14:30:40 -0600

    n/a

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.post('/text', async (req, res) => {
     .create({to: '+15164048254', from: '+13475072312', body: "There is an intruder!", mediaUrl: req.headers.host + `/images/${hashedName.jpg}`})
     .then((message) => console.log(message));
 
-  res.send('cool!');
+  res.end();
 });
 
 app

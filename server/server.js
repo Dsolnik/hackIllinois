68d0e188435587cbc3e3f5ed230a1365091900df
Daniel <dansolnik@gmail.com>
2018-02-25 08:56:00 -0600

    stuff

--- server/server.js
+++ server/server.js
@@ -73,18 +73,19 @@ app.post('/text', async(req, res) => {
   if (image) {
   const hashedName = image.hashCode();
   const base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
+  const filePath = path.join(__dirname, 'public','images',`${hashedName}.jpeg`);
 
-  await require("fs").writeFile(`/images/${hashedName}.jpeg`, base64Data, 'base64', function (err) {
+  await require("fs").writeFile(filePath, base64Data, 'base64', function (err) {
     console.log(err);
   });
-  console.log(req.headers.host + `/images/${hashedName}.jpeg`);
+  console.log(filePath);
 
   client
     .messages
     .create({
       to: `+1${number}`,
       from: '+13475072312',
-      mediaUrl: 'https://' + req.headers.host + `/images/${hashedName}.jpeg`
+      mediaUrl: filePath
     })
     .then((message) => console.log('image message succesfully sent!'))
     .catch((e) => console.log('error ', e));
